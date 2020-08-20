a2d75a890c848b698c25fb54bd56aa08879788b2
Robert Cunningham <robert@bagpyp.net>
2020-08-19 19:06:55 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='ghostinspector',
+    version='0.0.1',
+    description='tools for using the ghostInsepctor API',
+    py_modules=['ghostinspector'],
+    package_dir={'':'src'}
+)
